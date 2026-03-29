1bafd2119871a30deb58b4e36f93a631decb6458
Your Name <svayam996@gmail.com>
2026-03-29 17:51:18 +0530

    redis fix part 3

--- server/src/config/redis.js
+++ server/src/config/redis.js
@@ -1,20 +1,21 @@
 import "./env.js";
 import Redis from "ioredis";
 
-if (!process.env.REDIS_URL) {
-  throw new Error("REDIS_URL environment variable is not defined");
-}
-
-const connection = new Redis(process.env.REDIS_URL, {
+const connection = new Redis({
+  host: process.env.REDIS_HOST,
+  port: parseInt(process.env.REDIS_PORT),
+  username: process.env.REDIS_USERNAME || "default",
+  password: process.env.REDIS_PASSWORD,
+  tls: {
+    rejectUnauthorized: false,
+    servername: process.env.REDIS_HOST,
+  },
   maxRetriesPerRequest: null,
   connectTimeout: 30000,
   commandTimeout: 30000,
   socketTimeout: 30000,
   keepAlive: 10000,
   family: 0,
-  tls: {
-    rejectUnauthorized: false,
-  },
   retryStrategy(times) {
     if (times > 5) return null;
     return Math.min(times * 500, 3000);
@@ -28,6 +29,5 @@ connection.on("error", (err) => {
 connection.on("connect", () => {
   console.log("Redis connected successfully");
 });
-
 
 export default connection;
